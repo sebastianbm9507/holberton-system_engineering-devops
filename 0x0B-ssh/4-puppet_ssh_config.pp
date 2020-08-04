921c207d02b82c3fc2157b77b40046b4cce81786
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-08-04 19:46:38 +0000

    Hot fix

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 #Set config file
 file_line { 'Declaring entity file':
-    path => 'etc/ssh/ssh_config',
+    path => '/etc/ssh/ssh_config',
     line => 'IdentityFile ~/.ssh/holberton'
 }
 file_line { 'No autentication required':
-    path => 'etc/ssh/ssh_config',
-    line => 'PasswordAuthentication no'
+    path => '/etc/ssh/ssh_config',
+    line => 'PasswordAuthentication no',
 }
