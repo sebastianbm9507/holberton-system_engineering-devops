6b5189d2a8953c406550414a4613c66e8486b063
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-09-14 23:33:01 +0000

    Get todo list of an employee

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Script to have TODO list"""
+
+import requests
+import json
+import sys
+
+
+if __name__ == "__main__":
+    # user id
+    user_id = sys.argv[1]
+    # URLS to get data
+    todos_url = "https://jsonplaceholder.typicode.com/todos/"
+    employee_url = "https://jsonplaceholder.typicode.com/users/" + user_id
+    # Request
+    employee_todos = requests.get(todos_url)
+    employee_data = requests.get(employee_url)
+    # Converting into DICT
+    json_employee = employee_data.json()
+    json_todos = employee_todos.json()
+    # Extracting TODO of employee
+    total_todos = 0
+    done_todos = 0
+    done_tasks = []
+    for each_dict in json_todos:
+        if each_dict['userId'] == int(user_id):
+            if each_dict['completed']:
+                done_tasks.append(each_dict['title'])
+                done_todos += 1
+            total_todos += 1
+    # Ouputting the data
+    text = 'Employee {} is done with tasks({}/{})\
+'.format(json_employee['name'], done_todos, total_todos)
+    print(text)
+    for task in done_tasks:
+        print('\t{}'.format(task))
