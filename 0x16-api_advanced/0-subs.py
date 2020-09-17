f5c9fe313c3c5a198f8148a32facad668cf474e9
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-09-17 21:45:51 +0000

    Get subscribers for subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """ Get number_of_subscribers """
     URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     header = {'User-Agent': 'PostmanRuntime/7.26.5'}
     data = requests.get(URL, headers=header).json()
@@ -12,6 +13,6 @@ def number_of_subscribers(subreddit):
     if data.get('error') == 404:
         result = 0
     else:
-        data1 = (data.get('data').get('subscribers'))
+        data1 = data.get('data').get('subscribers')
         result = data1
     return result
