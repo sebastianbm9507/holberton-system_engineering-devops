5d1989daaa73af5cac0eeb9c0c4b18dd5198957e
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-09-17 21:44:38 +0000

    Get subscribers for dubreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Consume reddit API """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    URL = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    header = {'User-Agent': 'PostmanRuntime/7.26.5'}
+    data = requests.get(URL, headers=header).json()
+    result = None
+    if data.get('error') == 404:
+        result = 0
+    else:
+        data1 = (data.get('data').get('subscribers'))
+        result = data1
+    return result
