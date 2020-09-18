56925c0fe25523cd2c1819509d59bdefd18516a8
sebastianbm9507 <juan.bueno01@usc.edu.co>
2020-09-18 00:14:37 +0000

    Use recursion to get hot post from reddit API

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Consume reddit API recursively"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Get number_of_subscribers """
+    if not after:
+        URL = 'https://www.reddit.com/r/{}/hot.json?'.format(subreddit)
+    else:
+        URL = 'https://www.reddit.com/r/{}/hot.json?after={}'\
+             .format(subreddit, after)
+    header = {'User-Agent': 'PostmanRuntime/7.26.5'}
+    data = requests.get(URL, headers=header).json()
+    if data.get('error') == 403:
+        return(None)
+    else:
+        detailed_data = data.get('data').get('children')
+        for each_dict in detailed_data:
+            hot_list.append(each_dict.get('data').get('title'))
+        after = data.get('data').get('after')
+        if not after:
+            return hot_list
+        return recurse(subreddit, hot_list, after)
